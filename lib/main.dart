2b2c378657e677826da5a44610be6bfc3e8ce749
LuaSfg <108873347+lualys@users.noreply.github.com>
2025-10-18 21:26:40 -0300

    comiit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:device_preview/device_preview.dart';
+
 import 'models/time_model.dart';
 import 'pages/classificacao_page.dart';
 import 'pages/detalhes_time_page.dart';
@@ -7,11 +9,16 @@ import 'providers/times_provider.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => TimesProvider()..carregarTimes()),
-      ],
-      child: const BrasileiraoApp(),
+    DevicePreview(
+      enabled:
+          !const bool.fromEnvironment('dart.vm.product'), // ativa só em debug
+      builder: (context) => MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+              create: (_) => TimesProvider()..carregarTimes()),
+        ],
+        child: const BrasileiraoApp(),
+      ),
     ),
   );
 }
@@ -24,6 +31,11 @@ class BrasileiraoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Brasileirão 2025',
       debugShowCheckedModeBanner: false,
+      // 🔹 Integração com DevicePreview
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+
       theme: ThemeData(
         colorSchemeSeed: Colors.green,
         brightness: Brightness.light,
@@ -35,6 +47,7 @@ class BrasileiraoApp extends StatelessWidget {
         useMaterial3: true,
       ),
       themeMode: ThemeMode.system,
+
       initialRoute: '/',
       routes: {
         '/': (_) => const ClassificacaoPage(),
